52124f5dc4c5b7d003cb40a66cac4f55309f9410
Stefanny <imstefannyy@gmail.com>
2023-07-07 23:08:48 +0700

    resize the ongoing view

--- lib/transaction/transaction.dart
+++ lib/transaction/transaction.dart
@@ -47,142 +47,146 @@ class _TransactionState extends State<Transaction> {
             backgroundColor: Colors.white,
           ),
           body: TabBarView(children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(17, 20, 17, 0),
-              child: Container(
-                height: 155,
-                width: 350,
-                decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 8, top: 10, right: 8),
-                      child: Text(
-                        "28 April 2023",
-                        style: GoogleFonts.lexendDeca(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.grey[500],
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(17, 20, 17, 0),
+                child: Container(
+                  height: 155,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 8, top: 10, right: 8),
+                        child: Text(
+                          "28 April 2023",
+                          style: GoogleFonts.lexendDeca(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.grey[500],
+                          ),
                         ),
                       ),
-                    ),
-                    const Divider(
-                      thickness: 2,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                                flex: 0,
-                                child: Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 8, top: 3, right: 8),
-                                  child: Container(
-                                      height: 45,
-                                      width: 45,
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          color: const Color.fromRGBO(
-                                              13, 110, 253, 1)),
-                                      child: const Icon(
-                                        Icons.receipt,
-                                        color: Colors.white,
-                                      )),
-                                )),
-                            Expanded(
-                              flex: 3,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                      const Divider(
+                        thickness: 2,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                  flex: 0,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 8, top: 3, right: 8),
+                                    child: Container(
+                                        height: 45,
+                                        width: 45,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            color: const Color.fromRGBO(
+                                                13, 110, 253, 1)),
+                                        child: const Icon(
+                                          Icons.receipt,
+                                          color: Colors.white,
+                                        )),
+                                  )),
+                              Expanded(
+                                flex: 3,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Diding",
+                                      style: GoogleFonts.lexendDeca(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Isi Ulang Freon",
+                                      style: GoogleFonts.lexendDeca(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey,
+                                        fontSize: 12,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text(
-                                    "Diding",
-                                    style: GoogleFonts.lexendDeca(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16,
-                                    ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.only(left: 8),
+                                        child: Text(
+                                          "Total Harga",
+                                          style: GoogleFonts.lexendDeca(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                              color: Colors.grey[500]),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                          left: 8,
+                                        ),
+                                        child: Text(
+                                          "Rp 100,000",
+                                          style: GoogleFonts.lexendDeca(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  Text(
-                                    "Isi Ulang Freon",
-                                    style: GoogleFonts.lexendDeca(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey,
-                                      fontSize: 12,
-                                    ),
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.only(right: 8, top: 8),
+                                    child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              Color.fromRGBO(13, 110, 253, 1),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                          ),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                  builder: (_) =>
+                                                      DetailOngoing()));
+                                        },
+                                        child: Text("See Details")),
                                   )
                                 ],
                               ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8),
-                                      child: Text(
-                                        "Total Harga",
-                                        style: GoogleFonts.lexendDeca(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 12,
-                                            color: Colors.grey[500]),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                        left: 8,
-                                      ),
-                                      child: Text(
-                                        "Rp 100,000",
-                                        style: GoogleFonts.lexendDeca(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.only(right: 8, top: 8),
-                                  child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor:
-                                            Color.fromRGBO(13, 110, 253, 1),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10.0),
-                                        ),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.of(context).push(
-                                            MaterialPageRoute(
-                                                builder: (_) =>
-                                                    DetailOngoing()));
-                                      },
-                                      child: Text("See Details")),
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
